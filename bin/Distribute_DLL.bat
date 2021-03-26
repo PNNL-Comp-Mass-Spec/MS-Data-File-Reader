734099f5f1f50617bc10d2cb824217ab848366c4
alchemistmatt <matthew.monroe@pnnl.gov>
2021-03-26 16:29:49 -0700

    Add batch file

---
+++ bin/Distribute_DLL.bat
@@ -0,0 +1,28 @@
+
+xcopy MSDataFileReader.dll "F:\Documents\Projects\DataMining\CodeTest\Lib\MSDataFileReader.dll" /Y /D
+xcopy MSDataFileReader.dll "F:\Documents\Projects\DataMining\CodeTest\bin\MSDataFileReader.dll" /Y /D
+
+xcopy MSDataFileReader.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Common\MSDataFileReader.dll" /Y /D
+xcopy MSDataFileReader.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Program\bin\MSDataFileReader.dll" /Y /D
+xcopy MSDataFileReader.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_InSpecT_PlugIn\bin\MSDataFileReader.dll" /Y /D
+xcopy MSDataFileReader.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_DtaRefinery_PlugIn\bin\MSDataFileReader.dll" /Y /D
+xcopy MSDataFileReader.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_Masic_Plugin\bin\MSDataFileReader.dll" /Y /D
+
+xcopy MSDataFileReader.dll "F:\Documents\Projects\DataMining\MASIC\bin\MSDataFileReader.dll" /Y /D
+xcopy MSDataFileReader.dll "F:\Documents\Projects\DataMining\MASIC\Lib\MSDataFileReader.dll" /Y /D
+
+xcopy MSDataFileReader.dll "F:\Documents\Projects\DataMining\MSDataFileReaderDLL\Test_MSDataFileReader\bin\MSDataFileReader.dll" /Y /D
+xcopy MSDataFileReader.dll "F:\Documents\Projects\DataMining\MSDataFileTrimmer\bin\MSDataFileReader.dll" /Y /D
+xcopy MSDataFileReader.dll "F:\Documents\Projects\DataMining\MSDataFileTrimmer\Lib\MSDataFileReader.dll" /Y /D
+
+xcopy MSDataFileReader.dll "F:\Documents\Projects\DataMining\PepNovo_Results_Consolidator\Lib\MSDataFileReader.dll" /Y /D
+xcopy MSDataFileReader.dll "F:\Documents\Projects\DataMining\PepNovo_Results_Consolidator\Bin\MSDataFileReader.dll" /Y /D
+
+xcopy MSDataFileReader.dll "F:\Documents\Projects\DataMining\SpectrumLook\Inspect\MSDataFileReader.dll" /Y /D
+
+xcopy MSDataFileReader.dll "F:\Documents\Projects\DataMining\SpectrumLook_v2\lib\MSDataFileReader.dll" /Y /D
+
+xcopy MSDataFileReader.dll "F:\Documents\Projects\DataMining\CodeTestCS\lib\MSDataFileReader.dll" /Y /D
+xcopy MSDataFileReader.dll "F:\Documents\Projects\DataMining\CodeTestCS\bin\MSDataFileReader.dll" /Y /D
+
+pause
