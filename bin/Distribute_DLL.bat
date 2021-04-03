c269b02875637fadc93803aee2c37c1ea3009f51
alchemistmatt <matthew.monroe@pnnl.gov>
2021-04-03 12:50:04 -0700

    Update the distribution file

--- bin/Distribute_DLL.bat
+++ bin/Distribute_DLL.bat
@@ -1,28 +1,32 @@
 
-xcopy MSDataFileReader.dll "F:\Documents\Projects\DataMining\CodeTest\Lib\MSDataFileReader.dll" /Y /D
-xcopy MSDataFileReader.dll "F:\Documents\Projects\DataMining\CodeTest\bin\MSDataFileReader.dll" /Y /D
+xcopy MSDataFileReader.dll F:\Documents\Projects\DataMining\CodeTest\Lib\ /Y /D
+xcopy MSDataFileReader.dll F:\Documents\Projects\DataMining\CodeTest\bin\ /Y /D
 
-xcopy MSDataFileReader.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Common\MSDataFileReader.dll" /Y /D
-xcopy MSDataFileReader.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Program\bin\MSDataFileReader.dll" /Y /D
-xcopy MSDataFileReader.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_InSpecT_PlugIn\bin\MSDataFileReader.dll" /Y /D
-xcopy MSDataFileReader.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_DtaRefinery_PlugIn\bin\MSDataFileReader.dll" /Y /D
-xcopy MSDataFileReader.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_Masic_Plugin\bin\MSDataFileReader.dll" /Y /D
+xcopy MSDataFileReader.dll F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Common\ /Y /D
+xcopy MSDataFileReader.dll F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Program\bin\ /Y /D
+xcopy MSDataFileReader.dll F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_InSpecT_PlugIn\bin\ /Y /D
+xcopy MSDataFileReader.dll F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_DtaRefinery_PlugIn\bin\ /Y /D
+xcopy MSDataFileReader.dll F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_Masic_Plugin\bin\ /Y /D
 
-xcopy MSDataFileReader.dll "F:\Documents\Projects\DataMining\MASIC\bin\MSDataFileReader.dll" /Y /D
-xcopy MSDataFileReader.dll "F:\Documents\Projects\DataMining\MASIC\Lib\MSDataFileReader.dll" /Y /D
+xcopy MSDataFileReader.dll F:\Documents\Projects\DataMining\MASIC\bin\ /Y /D
+xcopy MSDataFileReader.dll F:\Documents\Projects\DataMining\MASIC\Lib\ /Y /D
 
-xcopy MSDataFileReader.dll "F:\Documents\Projects\DataMining\MSDataFileReaderDLL\Test_MSDataFileReader\bin\MSDataFileReader.dll" /Y /D
-xcopy MSDataFileReader.dll "F:\Documents\Projects\DataMining\MSDataFileTrimmer\bin\MSDataFileReader.dll" /Y /D
-xcopy MSDataFileReader.dll "F:\Documents\Projects\DataMining\MSDataFileTrimmer\Lib\MSDataFileReader.dll" /Y /D
+xcopy MSDataFileReader.dll F:\Documents\Projects\DataMining\MSDataFileReaderDLL\Test_MSDataFileReader\bin\ /Y /D
+xcopy MSDataFileReader.dll F:\Documents\Projects\DataMining\MSDataFileTrimmer\bin\ /Y /D
+xcopy MSDataFileReader.dll F:\Documents\Projects\DataMining\MSDataFileTrimmer\Lib\ /Y /D
 
-xcopy MSDataFileReader.dll "F:\Documents\Projects\DataMining\PepNovo_Results_Consolidator\Lib\MSDataFileReader.dll" /Y /D
-xcopy MSDataFileReader.dll "F:\Documents\Projects\DataMining\PepNovo_Results_Consolidator\Bin\MSDataFileReader.dll" /Y /D
+xcopy MSDataFileReader.dll F:\Documents\Projects\DataMining\PepNovo_Results_Consolidator\Lib\ /Y /D
+xcopy MSDataFileReader.dll F:\Documents\Projects\DataMining\PepNovo_Results_Consolidator\Bin\ /Y /D
 
-xcopy MSDataFileReader.dll "F:\Documents\Projects\DataMining\SpectrumLook\Inspect\MSDataFileReader.dll" /Y /D
+xcopy MSDataFileReader.dll F:\Documents\Projects\DataMining\SpectrumLook\Inspect\ /Y /D
 
-xcopy MSDataFileReader.dll "F:\Documents\Projects\DataMining\SpectrumLook_v2\lib\MSDataFileReader.dll" /Y /D
+xcopy MSDataFileReader.dll F:\Documents\Projects\DataMining\SpectrumLook_v2\lib\ /Y /D
 
-xcopy MSDataFileReader.dll "F:\Documents\Projects\DataMining\CodeTestCS\lib\MSDataFileReader.dll" /Y /D
-xcopy MSDataFileReader.dll "F:\Documents\Projects\DataMining\CodeTestCS\bin\MSDataFileReader.dll" /Y /D
+xcopy MSDataFileReader.dll F:\Documents\Projects\DataMining\CodeTestCS\lib\ /Y /D
+xcopy MSDataFileReader.dll F:\Documents\Projects\DataMining\CodeTestCS\bin\ /Y /D
+
+xcopy MSDataFileReader.dll F:\Documents\Projects\JoshAldrich\AScore\AScore_DLL\lib\ /Y /D
+xcopy MSDataFileReader.dll F:\Documents\Projects\JoshAldrich\AScore\AScore_DLL\bin\AnyCPU\Debug\ /Y /D
+xcopy MSDataFileReader.dll F:\Documents\Projects\JoshAldrich\AScore\AScore_Console\bin\Debug\ /Y /D
 
 pause
